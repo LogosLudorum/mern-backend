d07b88a1d7304561d72960d13da6e250eaeb0d1c
LogosLudorum <leunammesogol@gmail.com>
2022-10-27 21:40:27 +0300

    commented out API URL

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
-const apiURL = 'https://mernapp-logos-api.onrender.com'
+// const apiURL = 'https://mernapp-logos-api.onrender.com'
 
 // express app
 const app = express()
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.use(apiURL + '/api/workouts', workoutRoutes)
+app.use('/api/workouts', workoutRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
